74cd49a7a908a0425bea904d14a79190227c0273
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-09-04 22:42:45 +0600

    update

--- set-interval.js
+++ set-interval.js
@@ -1,6 +1,10 @@
+//
 let seconds = 0;
 const timeId = setInterval(()=>{
+    //starts with initial value
     console.log(seconds++);
+    //starts with increment value
+    console.log(++seconds);
     if(seconds === 10){
         clearInterval(timeId);
     }
