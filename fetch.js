76d8379d62467b9982ed6e68f8f12d83c437a2d4
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-09-08 16:27:58 +0600

    update

--- fetch.js
+++ fetch.js
@@ -1,6 +1,8 @@
 console.log(111111);
 console.log(222222);
+// setTimeout using asynchronous activity
 setTimeout(() => console.log('aaaaaaa'), 5000);
+// fetch using asynchronous activity
 fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
     .then(data => console.log(data))
--- setInterval.js
+++ setInterval.js
@@ -3,14 +3,22 @@ console.log('first');
 //     console.log('tik tik tik tik')
 // }, 1000)
 
-let seconds = 0;
+// let seconds = 0;
 
-const timeId = setInterval(() => {
-    // seconds++;
-    console.log(++seconds);
-    if (seconds > 15) {
-        clearInterval(timeId);
-    }
-}, 1000)
+// const timeId = setInterval(() => {
+//     // seconds++;
+//     console.log(++seconds);
+//     if (seconds > 15) {
+//         clearInterval(timeId);
+//     }
+// }, 1000)
 
 console.log('second');
+
+let num = 0;
+const number = setInterval(() => {
+    console.log(num = num + 5);
+    if(num === 50){
+        clearInterval(number);
+    }
+}, 1000);
